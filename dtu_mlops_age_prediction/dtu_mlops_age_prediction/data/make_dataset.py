6344e5e6293c47a91830cdd38d9a8849fa8c1671
Raquel Chaves <racha1601@gmail.com>
2024-01-10 16:31:46 +0100

    opening raw images
    Opening raw images and putting them into tensors

--- dtu_mlops_age_prediction/dtu_mlops_age_prediction/data/make_dataset.py
+++ dtu_mlops_age_prediction/dtu_mlops_age_prediction/data/make_dataset.py
@@ -1,3 +1,23 @@
+import os
+from pathlib import Path
+from torchvision import transforms
+from PIL import Image
+
 if __name__ == '__main__':
     # Get the data and process it
-    pass
+    path_in = Path('data/raw/face_age')
+    path_out = Path('data/processed')
+    labels = []
+    data = []
+    transformaciones = transforms.Compose([transforms.ToTensor(), ])
+    for folder in os.listdir(path_in):
+        path = Path(f'data/raw/face_age/{folder}')
+        for file in os.listdir(path):
+            image = Image.open(f'data/raw/face_age/{folder}/{file}')
+            data.append(transformaciones(image))
+            
+        cont = len(os.listdir(path))
+        labels += [folder]*cont
+        print(data)
+        #print(labels)
+        break
