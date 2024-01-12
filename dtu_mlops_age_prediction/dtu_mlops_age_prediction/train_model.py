1f4855403b21048aa1827a7a2aa3874afc0c3c99
Raquel Chaves <racha1601@gmail.com>
2024-01-12 12:23:41 +0100

    implementation of config file
    Use of config_train.yaml to set hyperparameters of train_model.py

--- dtu_mlops_age_prediction/dtu_mlops_age_prediction/train_model.py
+++ dtu_mlops_age_prediction/dtu_mlops_age_prediction/train_model.py
@@ -4,23 +4,28 @@ from pathlib import Path
 from torch import nn
 from models.model import age_predictor_model
 import numpy as np
+import hydra
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-@click.group()
+#@click.group()
 def cli():
     """Command line interface."""
     pass
 
-
+"""
 @click.command()
 @click.option("--lr", default=1e-3, help="learning rate to use for training")
 @click.option("--batch_size", default=64, help="batch size to use for training")
 @click.option("--num_epochs", default=5, help="number of epochs to train for")
-def train(lr, batch_size, num_epochs):
-    print(lr)
-    print(batch_size)
+"""
+
+@hydra.main(config_name="config_train.yaml")
+
+def info(cfg):
+    print(cfg.hyperparameters.learning_rate)
+    print(cfg.hyperparameters.batch_size)
 
 def import_data():
     path_in = 'data/processed'
@@ -40,36 +45,38 @@ def import_data():
     dataset = torch.utils.data.TensorDataset(torch.stack(train_data), train_label)
     return dataset
 
-batch_size = 128
+
+"""batch_size = 128
 num_epochs = 7
-lr = 1e-3
+lr = 1e-3"""
+def train(cfg):
+    model = age_predictor_model.to(device)
+    train_set = import_data()
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=cfg.hyperparameters.batch_size)
 
-model = age_predictor_model.to(device)
-train_set = import_data()
-train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+    optimizer = torch.optim.Adam(model.parameters(), lr=cfg.hyperparameters.learning_rate)
+    loss_fn = nn.CrossEntropyLoss()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-loss_fn = nn.CrossEntropyLoss()
+    for epoch in range(cfg.hyperparameters.num_epochs):
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            x, y = batch
+            x = x.to(device)
+            y = y.to(device)
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            loss.backward()
+            optimizer.step()
+        print(f"Epoch {epoch} Loss {loss}")
 
-for epoch in range(num_epochs):
-    for batch in train_dataloader:
-        optimizer.zero_grad()
-        x, y = batch
-        x = x.to(device)
-        y = y.to(device)
-        y_pred = model(x)
-        loss = loss_fn(y_pred, y)
-        loss.backward()
-        optimizer.step()
-    print(f"Epoch {epoch} Loss {loss}")
-
-torch.save(model, "models/model.pt")
+    torch.save(model, "models/model.pt")
 
 
 
-cli.add_command(train)
+#cli.add_command(train)
 
 
 
 if __name__ == "__main__":
-    cli()
+    train()
+    #cli()
