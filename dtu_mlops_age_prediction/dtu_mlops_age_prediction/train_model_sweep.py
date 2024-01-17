aa7cc881834a12ac582d2a22858ba53f7a08a586
Raquel Chaves <racha1601@gmail.com>
2024-01-17 17:19:16 +0100

    New information in wandb
    log of val_acc, train_acc

--- dtu_mlops_age_prediction/dtu_mlops_age_prediction/train_model_sweep.py
+++ dtu_mlops_age_prediction/dtu_mlops_age_prediction/train_model_sweep.py
@@ -12,31 +12,62 @@ from torch.profiler import profile, ProfilerActivity, tensorboard_trace_handler
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def import_data():
+def import_data(train = True):
+    
     path_in = os.getcwd()+'/data/processed'
-    train_data = torch.load(Path(path_in +'/train_data.pt'))
-    train_label = torch.load(Path(path_in + '/train_labels.pt'))
+    if train==True:
+        data = torch.load(Path(path_in +'/train_data.pt'))
+        label = torch.load(Path(path_in + '/train_labels.pt'))
+    else:
+        data = torch.load(Path(path_in +'/test_data.pt'))
+        label = torch.load(Path(path_in + '/test_labels.pt'))
 
-    label_to_index = {label: idx for idx, label in enumerate(set(train_label))}
-    train_label = [label_to_index[label] for label in train_label]
+    label_to_index = {lab: idx for idx, lab in enumerate(set(label))}
+    label = [label_to_index[lab] for lab in label]
 
     # Convert the list of tensors and numerical labels to PyTorch tensors
-    train_data = [torch.tensor(data) for data in train_data]
-    train_label = torch.tensor(train_label)
+    data = [torch.tensor(dat) for dat in data]
+    label = torch.tensor(label)
     
 
     # Create a PyTorch TensorDataset
-    dataset = torch.utils.data.TensorDataset(torch.stack(train_data), train_label)
+    dataset = torch.utils.data.TensorDataset(torch.stack(data), label)
     return dataset
+
+def calculate_validation_accuracy(model, val_dataloader):
+    model = age_predictor_model.to(device)
+    model.eval()
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for val_batch in val_dataloader:
+            val_x, val_y = val_batch
+            val_x = val_x.to(device)
+            val_y = val_y.to(device)
+            val_y_pred = model(val_x)
+            _, predicted = torch.max(val_y_pred.data, 1)
+            total += val_y.size(0)
+            correct += (predicted == val_y).sum().item()
+
+    accuracy = correct / total
+
+    return accuracy
 
 
 def train(learning_rate, batch_size, num_epochs):
     
     model = age_predictor_model.to(device)
+    model.train()
     wandb.watch(model, log_freq =100)
 
-    train_set = import_data()
+    #Importing train set
+    train_set = import_data(train=True)
     train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+
+    #Importing test set
+    val_set = import_data(train=False)
+    val_dataloader = torch.utils.data.DataLoader(val_set, batch_size=batch_size)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = nn.CrossEntropyLoss()
@@ -49,11 +80,23 @@ def train(learning_rate, batch_size, num_epochs):
             x = x.to(device)
             y = y.to(device)
             y_pred = model(x)
-            loss = loss_fn(y_pred, y)
-            loss.backward()
+            train_loss = loss_fn(y_pred, y)
+            train_loss.backward()
             optimizer.step()
-        print(f"Epoch {epoch} Loss {loss}")
-        wandb.log({'loss':loss})
+        #Log training loss
+        wandb.log({'train_loss': train_loss})
+
+        #Calculate and log training accuracy
+        train_acc = calculate_validation_accuracy(model, train_dataloader)
+        wandb.log({'train_acc': train_acc})
+        #Calculate and log validation accuracy
+        val_acc = calculate_validation_accuracy(model, val_dataloader)
+        wandb.log({'val_acc': val_acc})
+
+        #Log epoch
+        
+
+        print(f"Epoch {epoch} Train Loss {train_loss} Validation Accuracy {val_acc}")
 
     torch.save(model, "/models/model.pt")
 
