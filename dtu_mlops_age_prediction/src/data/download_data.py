97869fe388027f8ee83f0016ba8e81a798d657b5
Victor <viclunsant2000@gmail.com>
2024-01-18 21:49:02 +0100

    wandb experimentation

--- dtu_mlops_age_prediction/src/data/download_data.py
+++ dtu_mlops_age_prediction/src/data/download_data.py
@@ -2,6 +2,6 @@ import opendatasets as od
 
 
 def download_data(): 
-        od.download("https://www.kaggle.com/datasets/frabbisw/facial-age", data_dir = "./data/raw",)
+        od.download("https://www.kaggle.com/datasets/frabbisw/facial-age", data_dir = "./dtu_mlops_age_prediction/data/raw",)
 
 download_data()
